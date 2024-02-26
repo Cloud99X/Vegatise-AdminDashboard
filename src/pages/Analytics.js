886f831f10fec19d981d954ccc546a36a06e8848
Dilukshan <dilukshan.s@cloud99x.com>
2024-02-26 11:11:38 +0530

    newCampaign

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -48,7 +48,10 @@ const Analytics = () => {
     navigate("/log-in");
   }, [navigate]);
 
-
+  const handleNewCampaign = useCallback(() => {
+    navigate("/NewCampaign");
+  }, [navigate]);
+  
   return (
     <PageLayout activeSidebarItem="Analytics">
       <section className={styles.pageLayout}>
@@ -72,7 +75,9 @@ const Analytics = () => {
         </div>
         <div className={styles.bodyContainer}>
           <span className={styles.title}>Analytics</span>
-          <button className={styles.newCampaignContainer}>
+          <button 
+            onClick={handleNewCampaign}
+          className={styles.newCampaignContainer}>
             <img alt="plusIcon" src={plusIcon} className={styles.plusIcon} />
             <span className={styles.title2}>New Campaign</span>
           </button>
