3c2478c6ac2219566102afddc87cfb6b83a12007
Tharusha Perera <tharusha.p@cloud99x.com>
2024-02-27 09:14:28 +0530

    add environment variables

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DriversDataModule } from './drivers-data/divers-date.module';
+import * as admin from 'firebase-admin';
 
 @Module({
-  imports: [DriversDataModule],
+  imports: [ConfigModule.forRoot(), DriversDataModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    const firebaseAdminConfig = {
+      credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    };
+    admin.initializeApp(firebaseAdminConfig);
+  }
+}
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,28 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as admin from 'firebase-admin';
-import * as serviceAccount from './vegatise-1bad9-firebase-adminsdk-3p3md-91a17b07ba.json';
-
-const firebase_params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientX509CertUrl: serviceAccount.client_x509_cert_url,
-};
 
 async function bootstrap() {
   try {
     // Initialize Firebase Admin SDK
-    admin.initializeApp({
-      credential: admin.credential.cert(firebase_params),
-      storageBucket: 'vegatise-1bad9.appspot.com',
-    });
+    // admin.initializeApp({
+    //   credential: admin.credential.cert(firebase_params),
+    //   storageBucket: 'vegatise-1bad9.appspot.com',
+    // });
 
     // Create NestJS application
     const app = await NestFactory.create(AppModule);
@@ -32,7 +17,7 @@ async function bootstrap() {
     });
     // Start listening for incoming requests
     await app.listen(8000);
-    console.log('NestJS application is running on port 6000');
+    console.log('NestJS application is running on port 8000');
   } catch (error) {
     console.error('Error during bootstrap:', error);
     process.exit(1); // Exit the process with an error code
